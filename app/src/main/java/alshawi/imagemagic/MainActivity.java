173f0aabf74d66e7768634236884d56d4a649d58
Luay Alshawi sp3 <loay.vb@gmail.com>
2017-02-28 21:02:19 -0800

    support two orientation modes

--- app/src/main/java/alshawi/imagemagic/MainActivity.java
+++ app/src/main/java/alshawi/imagemagic/MainActivity.java
@@ -1,15 +1,21 @@
 package alshawi.imagemagic;
 
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
@@ -17,7 +23,7 @@ import org.opencv.imgproc.Imgproc;
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
     private static String TAG = "MainActivity";
     private JavaCameraView javaCameraView;
-    private Mat mRgba,imgGray;
+    private Mat mRgba,imgGray,mRgbaT,mRgbaF;
     private BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -41,10 +47,12 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
         javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
         javaCameraView.setVisibility(SurfaceView.VISIBLE);
         javaCameraView.setCvCameraViewListener(this);
+        javaCameraView.disableFpsMeter();
     }
 
     @Override
@@ -66,7 +74,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         {
             javaCameraView.disableView();
         }
-
     }
     @Override
     protected void onResume()
@@ -86,18 +93,31 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public void onCameraViewStarted(int width, int height) {
         mRgba = new Mat(height,width, CvType.CV_8UC4);
+        mRgbaT = new Mat(height,width, CvType.CV_8UC4);
+        mRgbaF = new Mat(height,width, CvType.CV_8UC4);
         imgGray = new Mat(height,width, CvType.CV_8UC1);
     }
 
     @Override
     public void onCameraViewStopped() {
         mRgba.release();
+        imgGray.release();
+        mRgbaT.release();
+        mRgbaF.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mRgba = inputFrame.rgba();
-        Imgproc.cvtColor(mRgba,imgGray, Imgproc.COLOR_RGB2GRAY);
-        return imgGray;
+        //convert rgb to gray
+        //Imgproc.cvtColor(mRgba,imgGray, Imgproc.COLOR_RGB2GRAY);
+        int rotation =  getWindowManager().getDefaultDisplay().getRotation();
+        if(rotation == 0)
+        {
+            Core.transpose(mRgba, mRgbaT);
+            Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0,0, 0);
+            Core.flip(mRgbaF, mRgba, 1 );
+        }
+        return mRgba;
     }
 }
